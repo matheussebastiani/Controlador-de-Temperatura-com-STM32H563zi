5cb7100113d902e9ecdc06bb69ecd1b91785bd46
Matheus <matheus.nmt@gmail.com>
2025-11-22 16:02:36 -0300

    iniciado o projeto no stm32 + implementação leitura lm35

---
+++ src/Core/Inc/lm35.h
@@ -0,0 +1,41 @@
+/*
+ * lm35.h
+ *
+ *  Created on: Nov 22, 2025
+ *      Author: matheus, gustavo
+ *
+ *  Neste arquivo contém: definição de constantes e funções para a leitura do LM35 e conversão de
+ *  volts para °C
+ */
+
+#ifndef INC_LM35_H_
+#define INC_LM35_H_
+
+/* Includes */
+
+#include "adc.h"
+
+/* Includes */
+
+#define RESOLUCAO_ADC_LM35 		  4095.0
+#define TENSAO_REFERENCIA_AD 	  3.3
+#define FATOR_CONVERSAO_LM35_VOLS 100.0 		// Como 1°C = 10mV
+#define NUM_MEDICOES 			  10.0
+
+/* Variáveis Exportadas */
+
+// Isso permitirá utilizar essa variável em outros arquivos, apenas incluindo o lm35.h
+
+extern float temperaturaAtual;
+
+/* Variáveis Exportadas */
+
+/* Funções do LM35: */
+
+float calculaTemperatura();
+
+float ADCtoVolts(ADC_HandleTypeDef *adc_channel);
+
+/* _Funções do LM35 */
+
+#endif /* INC_LM35_H_ */
---
+++ src/Core/Inc/nextion.h
@@ -0,0 +1,48 @@
+/*
+ * nextion.h
+ *
+ *  Created on: Oct 31, 2025
+ *      Author: julio
+ */
+
+#ifndef NEXTION_H
+#define NEXTION_H
+
+#include "main.h"
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+// Estrutura para dados do touch event
+typedef struct
+{
+	uint8_t page_id;
+	uint8_t component_id;
+	uint8_t event_type;
+} nextion_touch_event_t;
+
+
+typedef struct
+{
+	int8_t active_page;
+	nextion_touch_event_t touch_event;
+}nextion_ihm_t;
+
+//variaveis globais
+extern nextion_ihm_t nextion_ihm;
+
+
+void nextion_init();
+void nex_send_cmd(const char *cmd);
+void nextion_get_active_page();
+void nextion_parse_command(uint8_t *data, uint16_t size);
+// Funções auxiliares para interface com o Nextion
+void nextion_set_page(uint8_t page_id);
+void nextion_set_component_color(const char *component, uint32_t value);
+void nextion_set_component_value(const char *component, uint32_t value);
+void nextion_set_component_text(const char *component, const char *text);
+void nextion_move_component(const char *component_name, int16_t x, int16_t y);
+void nextion_set_background_color(const char *page_name, uint32_t value);
+
+#endif /* NEXTION_H */
+
